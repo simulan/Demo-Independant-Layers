9348757ce57793515f951a43c0f8e6e40b450d34
simulan <simulan88@yahoo.com>
2017-07-05 15:50:08 +0200

    removed UI Unit Tests from ThreadsAdapterTest

--- app/src/test/java/be/simulan/reddit_demo/mvp/views/adapters/ThreadsAdapterTest.kt
+++ app/src/test/java/be/simulan/reddit_demo/mvp/views/adapters/ThreadsAdapterTest.kt
@@ -1,34 +1,10 @@
 package be.simulan.reddit_demo.mvp.views.adapters
 
-import android.support.v7.widget.RecyclerView
 import be.simulan.reddit_demo.AndroidTest
-import be.simulan.reddit_demo.R
 import be.simulan.reddit_demo.mvp.models.data.RThread
-import be.simulan.reddit_demo.mvp.views.MainActivity
-import be.simulan.reddit_demo.mvp.views.adapters.viewholders.ThreadViewHolder
 import org.junit.Test
-import org.robolectric.Robolectric
 
 class ThreadsAdapterTest : AndroidTest() {
-
-    @Test fun getViewHolder() {
-        val activity = Robolectric.setupActivity(MainActivity::class.java)
-        val adapter = ThreadsAdapter()
-        val recycler = activity.findViewById(R.id.recycler) as RecyclerView
-        val layoutId = R.layout.row_thread
-        val thread = RThread()
-        thread.title = "threadName"
-        recycler.adapter = adapter
-        val list = listOf(thread)
-        adapter.add(list)
-
-        try {
-            val vh: ThreadViewHolder = adapter.onCreateViewHolder(recycler, layoutId) as ThreadViewHolder
-            assert(vh is ThreadViewHolder)
-        } catch(exception: Exception) {
-            assert(false)
-        }
-    }
     @Test fun getItemCount() {
         val adapter = ThreadsAdapter()
         assert(adapter.itemCount == 0)
