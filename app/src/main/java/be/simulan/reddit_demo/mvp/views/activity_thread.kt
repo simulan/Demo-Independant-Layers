c0d9b535ea107f4648b0a122054c2e5f8536b81a
simulan <simulan88@yahoo.com>
2017-08-20 13:45:49 +0200

    stub ThreadView

---
+++ app/src/main/java/be/simulan/reddit_demo/mvp/views/activity_thread.kt
@@ -0,0 +1,13 @@
+package be.simulan.reddit_demo.mvp.views
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import be.simulan.reddit_demo.R
+
+class activity_thread : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thread)
+    }
+}
